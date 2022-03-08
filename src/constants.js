02817fa7b900c2bec41dc92757e342f1aebb5869
David But <97487385+DavidBut505@users.noreply.github.com>
2022-03-08 15:21:09 +0100

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const ERC20_DECIMALS = 18
+const contractAddress = "0x0ea05DD410b97517CB1b70010525B5B7e0b30CDA"
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+
+export { ERC20_DECIMALS, contractAddress, cUSDContractAddress };
